006df5eafa4c4b20a9fe3bc338f243a47dfb032c
Victor Potasso <victorpotasso@gmail.com>
2019-03-30 16:26:46 +1300

    updates npm manifest

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { useQueue, getQueues, getQueuesByKey } from "./lib/index.js";
 
-async function main() {
+function main() {
   const outputEl = document.querySelector('.output');
   const progressEl = document.querySelector('.progress');
 
